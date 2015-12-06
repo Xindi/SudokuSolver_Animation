f24a9a3f04d01ecf81796f67c2202a37bbcd551c
Xindi <xg73@cornell.edu>
2015-12-06 17:38:59 -0500

    add table border

--- brute_force.js
+++ brute_force.js
@@ -15,10 +15,10 @@ var easy_boards = [
 
 function brute_force(board) {
     var dimension = board.length;
-    for (row = 0; i < dimension; row++) { 
-    	for (col = 0; col < dimension; col++) {
+    for (var row = 0; row < dimension; row++) { 
+    	for (var col = 0; col < dimension; col++) {
     		if (board[row][col] == ".") {
-    			for (i = 1; i < dimension+1; i++) {
+    			for (var i = 1; i < dimension+1; i++) {
     				board[row][col] = i.toString();
     				if( board_is_valid(board, row, col) && brute_force(board)) {
     					return true;
@@ -35,14 +35,14 @@ function brute_force(board) {
 function board_is_valid(board, row, col) {
 	var dimension = board.length;
 	// check row
-	for (i = 0; i < dimension; i++) {
+	for (var i = 0; i < dimension; i++) {
 		if( i != col && board[row][i] == board[row][col] ) {
 			return false;
 		}
 	}
 
 	// check column
-	for (j = 0; j < dimension; j++) {
+	for (var j = 0; j < dimension; j++) {
 		if( j != row && board[j][col] == board[row][col]) {
 			return false;
 		}
@@ -51,8 +51,8 @@ function board_is_valid(board, row, col) {
 	// check square
 	var rSquare = row / 3;
 	var cSquare = col / 3;
-	for (i = rSquare*3; i < rSquare*3+3; i++) {
-		for (j = rSquare*3; j < rSquare*3+3; j++) {
+	for (var i = rSquare*3; i < rSquare*3+3; i++) {
+		for (var j = rSquare*3; j < rSquare*3+3; j++) {
 			if( !(i == row && j == col) && (board[i][j] == board[row][col]) ) {
 				return false;
 			}
